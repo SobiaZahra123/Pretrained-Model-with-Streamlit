6a5ccc8ee17943bc98998c7c29dad6fe1bf7c830
Sobia Zahra <sobiazahra2006@gmail.com>
2026-06-29 09:01:12 -0700

    Update app.py

--- app.py
+++ app.py
@@ -206,8 +206,8 @@ cmap = plt.colormaps.get_cmap('tab10')
 cmap = plt.get_cmap('tab10')  
 colors3 = [cmap(i) for i in range(n)]
 
-    fig4 = plt.figure(figsize=(10, 7))
-    ax4  = fig4.add_subplot(111, projection='3d')
+fig4 = plt.figure(figsize=(10, 7))
+ax4  = fig4.add_subplot(111, projection='3d')
 
     for i in range(n):
         ax4.scatter(
